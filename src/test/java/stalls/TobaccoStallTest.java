ddd7f2ed86bae5f9f13cc404a6058d9057ddcfd3
idmacleod <idmacleod@gmail.com>
2020-06-23 16:46:29 +0100

    Added security tests to TobaccoStall

--- src/test/java/stalls/TobaccoStallTest.java
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -2,8 +2,10 @@ package stalls;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
+import sun.jvm.hotspot.runtime.StaticBaseConstructor;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class TobaccoStallTest {
 
@@ -27,5 +29,17 @@ public class TobaccoStallTest {
     @Test
     public void hasParkingSpot() {
         assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
+    }
+
+    @Test
+    public void canCheckIfAllowedToVisitor__true() {
+        Visitor visitor = new Visitor(25, 1.7, 100);
+        assertTrue(tobaccoStall.isAllowedTo(visitor));
+    }
+
+    @Test
+    public void canCheckIfAllowedToVisitor__false() {
+        Visitor visitor = new Visitor(14, 1.2, 40);
+        assertFalse(tobaccoStall.isAllowedTo(visitor));
     }
 }
