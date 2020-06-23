fbda8a42b24e95c2577483b474dde8d7323e26e3
idmacleod <idmacleod@gmail.com>
2020-06-23 18:16:33 +0100

    Added tests for visited attractions

--- src/test/java/people/VisitorTest.java
+++ src/test/java/people/VisitorTest.java
@@ -1,17 +1,19 @@
 package people;
 
+import attractions.Dodgems;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class VisitorTest {
-
-    Visitor visitor;
+    private Visitor visitor;
+    private Dodgems dodgems;
 
     @Before
     public void before(){
         visitor = new Visitor(14, 1.2, 40.0);
+        dodgems = new Dodgems("Bumper Cars", 5);
     }
 
     @Test
@@ -27,5 +29,16 @@ public class VisitorTest {
     @Test
     public void hasMoney() {
         assertEquals(40.0, visitor.getMoney(), 0.1);
+    }
+
+    @Test
+    public void visitedAttractionsStartsEmpty() {
+        assertEquals(0, visitor.getVisitedAttractions().size());
+    }
+
+    @Test
+    public void canAddToVisited() {
+        visitor.addToVisited(dodgems);
+        assertEquals(1, visitor.getVisitedAttractions().size());
     }
 }
