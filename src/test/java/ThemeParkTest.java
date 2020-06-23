beb9d0eb5d350972f38369ed83ae8bfab99ffd20
idmacleod <idmacleod@gmail.com>
2020-06-23 19:27:20 +0100

    Added test to ensure visitors pay for ticketed events

--- src/test/java/ThemeParkTest.java
+++ src/test/java/ThemeParkTest.java
@@ -53,11 +53,12 @@ public class ThemeParkTest {
     }
 
     @Test
-    public void canLogVisit() {
+    public void canVisit() {
         Visitor visitor = new Visitor(25, 1.7, 100.0);
         themePark.visit(visitor, park);
         assertEquals(1, park.getVisitCount());
         assertEquals(1, visitor.getVisitedAttractions().size());
+        assertEquals(91.60, visitor.getMoney(), 0.01);
     }
 
     @Test
