779ed6a88685156bf50329fae324639f1e84c490
idmacleod <idmacleod@gmail.com>
2020-06-23 17:39:14 +0100

    MVP complete

--- src/main/java/attractions/Dodgems.java
+++ src/main/java/attractions/Dodgems.java
@@ -1,8 +1,9 @@
 package attractions;
 
+import behaviours.ITicketed;
 import people.Visitor;
 
-public class Dodgems extends Attraction {
+public class Dodgems extends Attraction implements ITicketed {
     public Dodgems(String name, int rating) {
         super(name, rating);
     }
